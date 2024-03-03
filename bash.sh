2b02fc327e3197de0c9a7fdff434182fd85e3c29
deepak suthar <deepaksuthar40128@gmail.com>
2024-03-03 15:55:00 +0530

    bash cmds

--- bash.sh
+++ bash.sh
@@ -2,10 +2,20 @@
 
 cd ./client
 
+echo "running npm install in client"
 npm install
+echo "package installation complete"
 
-npm run build
+echo "Running client..."
+npm run dev &
+echo "Client Started at http://localhost:5173"
 
 cd ../
 
-echo "Lets go..."
+echo "running npm install in server"
+npm install
+echo "package installation complete"
+
+echo "Running server..."
+npm run dev &
+echo "Server Started at http://localhost:4320"
